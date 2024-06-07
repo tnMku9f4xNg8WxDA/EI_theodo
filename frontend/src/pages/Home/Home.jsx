d75cdffb8bd7a40eba38df6d9d24e4149134b510
Poncel Gael <gael.poncel@student-cs.fr>
2024-06-07 12:20:37 +0200

    select user home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import './Home.css';
 import { useState } from 'react';
 import loupe_recherche from './loupe_recherche.png';
 import MovieTable from '../../components/MovieTable/MovieTable';
+import { useFetchUsers } from './../Users/useFetchUsers';
 
 function Home() {
   const [movieName, setmovieName] = useState('');
@@ -9,23 +10,62 @@ function Home() {
   const savemovieName = (event) => {
     setmovieName(event.target.value);
   };
+  const { users, usersLoadingError, fetchUsers } = useFetchUsers();
+  const [userEmail, setUserEmail] = useState('');
+  const [userFirstname, setUserFirstname] = useState('Choisissez un');
+  const [userLastname, setUserLastname] = useState('utilisateur !');
+  const [userID, setUserID] = useState(0);
+
+  const setInfosUser = (email, firstname, lastname, id) => {
+    setUserEmail('(' + email + ')');
+    setUserFirstname('Vous êtes ' + firstname);
+    setUserLastname(lastname);
+    setUserID(id);
+  };
 
   return (
     <div className="App">
-      <header>
-        <div className="search">
-          <img className="imageSearch" src={loupe_recherche} alt="loupe" />
-          <input
-            type="text"
-            placeholder="Rechercher un film"
-            value={movieName}
-            onChange={savemovieName}
-          />
-        </div>
-        <div className="App-header">
-          <MovieTable name={movieName} />
-        </div>
-      </header>
+      <h2>
+        {userFirstname} {userLastname} {userEmail}
+      </h2>
+      <nav>
+        <ul>
+          <li class="deroulant">
+            <a href="#">Utilisateurs &ensp;</a>
+            <ul class="sous">
+              {users.map((user) => (
+                <li key={user.email}>
+                  <a
+                    href="#"
+                    onClick={() => {
+                      setInfosUser(
+                        user.email,
+                        user.firstname,
+                        user.lastname,
+                        user.id
+                      );
+                    }}
+                  >
+                    {user.email}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <div className="search">
+        <img className="imageSearch" src={loupe_recherche} alt="loupe" />
+        <input
+          type="text"
+          placeholder="Rechercher un film"
+          value={movieName}
+          onChange={savemovieName}
+        />
+      </div>
+      <div className="App-header">
+        <MovieTable name={movieName} />
+      </div>
     </div>
   );
 }
